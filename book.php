e0212a04e2df2dff9dfe1ee0dbaf04cfe723258a
Seobs <subi88@naver.com>
2016-12-26 14:49:40 +0900

    change log name

--- book.php
+++ book.php
@@ -5,4 +5,4 @@ $m = $_POST['m'];
 $date = date('Y-m-d h:i:s');
 $log_msg = $date.'  |  '.$t.'  |  '.$m;
 
-error_log($log_msg.PHP_EOL, 3, './log/book.log');
+error_log($log_msg.PHP_EOL, 3, './log/booking_list.log');
